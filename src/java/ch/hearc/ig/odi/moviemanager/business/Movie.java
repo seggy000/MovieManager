c84b582953e05fc0f3fd4189178dc3db005aaa75
Seggy <thomas.ruegsegger@he-arc.ch>
2015-12-10 09:05:49 +0100

    Ajout des méthodes pour ajouter les éléments dans les listes de chaque classe + commentaires

--- src/java/ch/hearc/ig/odi/moviemanager/business/Movie.java
+++ src/java/ch/hearc/ig/odi/moviemanager/business/Movie.java
@@ -1,7 +1,15 @@
 package ch.hearc.ig.odi.moviemanager.business;
 
+import ch.hearc.ig.odi.moviemanager.exception.UnicityException;
 import java.util.*;
 
+/**
+ * 
+ * @author seggy
+ * 
+ * A recorded sequence of film or video images displayed on a screen with sufficient rapidity as to create the illusion of motion and continuity.
+ * Can also contain aural stimulation to immerge the Person watching it.
+ */
 public class Movie {
 
 	Collection<Person> people;
@@ -9,7 +17,25 @@ public class Movie {
 	private String name;
 	private String producer;
 
-	public Long getId() {
+        /**
+         * 
+         * @param id
+         * @param name
+         * @param producer 
+         * 
+         * I hope this one will be good when finished
+         */
+        public Movie(Long id, String name, String producer) {
+            this.id = id;
+            this.name = name;
+            this.producer = producer;
+            this.people = new ArrayList<>();
+        }
+
+        
+
+	
+        public Long getId() {
 		return this.id;
 	}
 
@@ -44,5 +70,22 @@ public class Movie {
 	public void setProducer(String producer) {
 		this.producer = producer;
 	}
+
+        
+    /**
+     * 
+     * @param person
+     * @throws UnicityException 
+     * 
+     * Adds a Person to the "this movie has been watched by" list
+     */
+    void addPerson(Person person) throws UnicityException {
+        if(this.people.contains(person)){
+            throw new UnicityException();
+        }
+        
+        this.people.add(person);
+        person.addMovie(this);
+    }
 
 }
--- src/java/ch/hearc/ig/odi/moviemanager/business/Person.java
+++ src/java/ch/hearc/ig/odi/moviemanager/business/Person.java
@@ -1,48 +1,88 @@
 package ch.hearc.ig.odi.moviemanager.business;
 
+import ch.hearc.ig.odi.moviemanager.exception.UnicityException;
 import java.util.*;
 
+/**
+ * @author Seggy
+ *
+ * An individual human being
+ * 
+*/
 public class Person {
 
-	Collection<Movie> movies;
-	private Long id;
-	private String firstName;
-	private String lastname;
+    Collection<Movie> movies;
+    private Long id;
+    private String firstName;
+    private String lastname;
 
-	public Long getId() {
-		return this.id;
-	}
+    /**
+     *
+     * @param id
+     * @param firstName
+     * @param lastname
+     *
+     * The JVM (God) gives life to a person when he invoques this magnificent function
+     */
+    public Person(Long id, String firstName, String lastname) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastname = lastname;
+        
+        //Creates a new list of movies for the newborn
+        this.movies = new ArrayList<>();
+    }
 
-	/**
-	 * 
-	 * @param id
-	 */
-	public void setId(Long id) {
-		this.id = id;
-	}
+    /**
+     * 
+     * @param movie 
+     * 
+     * Adds a movie to the person's personal "Watched list"
+     */
+    public void addMovie(Movie movie) throws UnicityException {
+        
+        if(this.movies.contains(movie)){
+            throw new UnicityException();
+        }
+        
+        this.movies.add(movie);
+        movie.addPerson(this);
+    }
 
-	public String getFirstName() {
-		return this.firstName;
-	}
+    public Long getId() {
+        return this.id;
+    }
 
-	/**
-	 * 
-	 * @param firstName
-	 */
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    /**
+     *
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getLastname() {
-		return this.lastname;
-	}
+    public String getFirstName() {
+        return this.firstName;
+    }
 
-	/**
-	 * 
-	 * @param lastname
-	 */
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
+    /**
+     *
+     * @param firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return this.lastname;
+    }
+
+    /**
+     *
+     * @param lastname
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
 }
